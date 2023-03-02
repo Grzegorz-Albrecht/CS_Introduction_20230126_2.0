ea4e8e891fd43634d50ffef6295a6b92ee16fa02
Your Name <you@example.com>
2023-03-02 22:02:12 +0500

    Lesson 1 02

---
+++ 007/Program.cs
@@ -0,0 +1,15 @@
+﻿// 7.Выяснить является ли число чётным.
+int N2;
+string s;
+    System.Console.Write("Введите N2: ");
+    s=Console.ReadLine();
+    N2=Convert.ToInt32(s);
+    bool odd= N2%2==0;
+    if (odd==true)
+    {
+        System.Console.WriteLine($"Число {N2} четное ");
+    }
+    else  
+    {
+         System.Console.WriteLine($"Число {N2} не четное ");
+    }
