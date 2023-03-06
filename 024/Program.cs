d3dd6caa0192a7728dbea4387fcf46c5851970c1
Your Name <you@example.com>
2023-03-07 01:03:22 +0500

    Lesson 2.2

---
+++ 024/Program.cs
@@ -0,0 +1,16 @@
+﻿// 24. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+Console.Write("Введите координаты x ");
+int x=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты y ");
+int y=Convert.ToInt32(Console.ReadLine());
+if (x>0 & y>0)
+    Console.WriteLine("Первая четверть");
+    else 
+if (x<0 & y>0)
+    Console.WriteLine("Вторая четверть");
+    else 
+if (x<0 & y<0)
+    Console.WriteLine("Третья четверть");
+    else 
+if (x>0 & y<0)
+    Console.WriteLine("Четвертая четверть");
---
+++ 027/Program.cs
@@ -0,0 +1,9 @@
+﻿// 27. Найти расстояние между точками в пространстве 2D
+Console.WriteLine("Введите координаты");
+double x1=Convert.ToDouble(Console.ReadLine());
+double x2=Convert.ToDouble(Console.ReadLine());
+double y1=Convert.ToDouble(Console.ReadLine());
+double y2=Convert.ToDouble(Console.ReadLine());
+double n = Convert.ToDouble(Math.Pow((x2-x1), 2)+Math.Pow((y2-y1), 2));
+double S=Convert.ToDouble(Math.Sqrt(n)); //расстояние 
+Console.Write($"расстояние между точками {S}"); 
