674d23f57eec792af48c884a7aef130514238b00
Your Name <you@example.com>
2023-03-06 21:58:02 +0500

    Lesson 2

---
+++ 020/Program.cs
@@ -0,0 +1,7 @@
+﻿// 20. Дано число. Проверить кратно ли оно 7 и 23
+int a=16154;
+if 
+    (a%7==0 & a%23==0)
+    Console.WriteLine($" {a} кратно 7 и 23");
+else
+    System.Console.WriteLine("NO");
---
+++ 022/Program.cs
@@ -0,0 +1,9 @@
+﻿// 22. По двум заданным числам проверять является ли одно квадратом другого.
+double a=12,b=144;
+if (a*a==b)
+Console.WriteLine("b является квадратом a");
+else 
+if (b*b==a)
+Console.WriteLine("a является квадратом b");
+else 
+Console.WriteLine("не является квадратом");
